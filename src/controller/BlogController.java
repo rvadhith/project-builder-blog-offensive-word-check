23768dc5d3dc280aa9f2713ceeb1fe7128f25882
Adhithya <rvadhith@gmail.com>
2020-04-23 08:11:49 +0530

    Test file issues

--- src/controller/BlogController.java
+++ src/controller/BlogController.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import model.Blog;
 import model.User;
+import utility.CheckBlogPost;
 
 
 @WebServlet(urlPatterns= {"/blog"})
@@ -47,8 +48,12 @@ public class BlogController extends HttpServlet {
 		blog.setBlogTitle(title);
 		blog.setBlogDescription(description);
 		blog.setDate(postedOn);
-
-		if(checkblog!) {
+		
+		//Creation of object for CheckBlogPost
+		CheckBlogPost post = new CheckBlogPost(); 
+		boolean checkblog = post.checkBlog(blog);
+		
+		if(checkblog == true) {
 			request.setAttribute("blog", blog);
 			request.setAttribute("user",user);
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/blogView.jsp");
--- src/utility/CheckBlogPost.java
+++ src/utility/CheckBlogPost.java
@@ -1,1 +1,47 @@
 package utility;
+
+import model.Blog;
+
+public class CheckBlogPost extends OffensiveWordsChecker implements OffensiveWordsInterface {
+
+	String offensiveWords[] = {"a55", "a55hole", "acrotomophilia", "aeolus", "ahole", "alabama hot pocket", "alaskan pipeline", "anal", "anal impaler", "anal leakage",	"analprobe", "anilingus", "anus", "apeshit", "ar5e", "areola", "areole", "arian", "arrse", "arse", "arsehole", "aryan", "ass", "ass fuck", "ass hole"};
+	
+	
+	@Override
+	public boolean checkBlogTitle(Blog blog) {
+		String blogTitle = blog.getBlogTitle().toLowerCase();
+		String wordsInTitle[] = blogTitle.split(" ") ;
+		for(int i = 0; i < 25; i++) {
+			for(int j= 0; j < wordsInTitle.length; j++) {
+				if(wordsInTitle[j] == offensiveWords[i]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public boolean checkBlogDescription(Blog blog) {
+		String blogDescription = blog.getBlogDescription().toLowerCase();
+		String wordsInDescription[] = blogDescription.split(" ") ;
+		for(int i = 0; i < 25; i++) {
+			for(int j= 0; j < wordsInDescription.length; j++) {
+				if(wordsInDescription[j] == offensiveWords[i]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+		
+
+	@Override
+	public boolean checkBlog(Blog blog) {
+		if((checkBlogTitle(blog)) && (checkBlogDescription(blog))) {
+			return true;
+		}
+		return false;
+	}
+	
+}
